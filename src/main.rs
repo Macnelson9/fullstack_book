bc0d46e059bddb1c7c613679b2e2c9d95e5ea495
macnelson9 <michaelofatu@gmail.com>
2026-02-07 22:22:13 +0100

    feat: implement binary and library crates

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+// fn print_number(num: Option<i32>) {
+//     match num {
+//         Some(n) => println!("I see {}!", n),
+//         None => println!("I see nothing!"),
+//     }
+// }
+
+// fn main() {
+//     let x = Some(42);
+//     let y = None;
+//     print_number(x);
+//     print_number(y);
+// }
+
+fn main() {
+    rust_saturdays::print();
+}
+
+// For loop in rust 
+// for variable in iterator {
+//    body of the loop
+// }
