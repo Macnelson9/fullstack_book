612b8430cc866f7e024fdc188c439ddb6e0da81e
macnelson9 <michaelofatu@gmail.com>
2026-02-08 23:17:37 +0100

    feat: implement working with vectors

--- src/lib.rs
+++ src/lib.rs
@@ -2,11 +2,34 @@ pub fn say_hello () {
     println!("Hello, world!");
 }
 
+// Print function using arrays
+// pub fn print() {
+//     let numbers = [1,2,3,4,5];
+
+//     let () = numbers;
+
+//     for n in numbers.iter() {
+//         println!("{}", n);
+//     }
+// }
+
+// Print function using vector 
+// pub fn print() {
+//     let numbers = vec![1,2,3,4,5];
+
+//     for n in numbers {
+//         println!("{}", n);
+//     }
+// }
+
+// Print function using a helper function
 pub fn print() {
     let numbers = [1,2,3,4,5];
 
-    let () = numbers;
+    output_sequence(numbers);
+}
 
+fn output_sequence(numbers: [u8; 5]) {
     for n in numbers.iter() {
         println!("{}", n);
     }
